8fb06f3cbf8fd2de4f2c764c9221d86abecd7722
unknown <k2sourav@yahoo.com>
2018-11-25 21:06:12 +0530

    initial commit 2

---
+++ build/precache-manifest.9be7df7ba88ddfd485ef2d41f3543fdf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee410cb4c2c09a055d25",
+    "url": "/static/js/main.ee410cb4.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "4daa2f420907b1a0f6846b3c8096612e",
+    "url": "/index.html"
+  }
+];
