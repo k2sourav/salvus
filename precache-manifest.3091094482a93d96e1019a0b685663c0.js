359c93eca1d174e56ab23f2960aea343cd4b159d
unknown <k2sourav@yahoo.com>
2019-08-11 21:58:38 +0530

    Updates

---
+++ precache-manifest.3091094482a93d96e1019a0b685663c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2b1d2ab702166df3871",
+    "url": "/salvus/static/js/runtime~main.b2b1d2ab.js"
+  },
+  {
+    "revision": "00c85136da2925980943",
+    "url": "/salvus/static/js/main.00c85136.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/salvus/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "e82b34d04a2f92381144b27367f00407",
+    "url": "/salvus/index.html"
+  }
+];
